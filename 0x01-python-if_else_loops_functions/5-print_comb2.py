41e0d057e7cd5271afcc5f52df5a0c68f9b851fe
Olatunji Sobayo <sobayolatunji@gmail.com>
2022-08-23 21:46:41 +0100

    Delete 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++
@@ -1,5 +0,0 @@
-for i in range(100):
-    if i < 99:
-        print('{}'.format('0' * (2 - len(str(i))) + str(i)), end=', ')
-    elif i == 99:
-        print('{}'.format('0' * (2 - len(str(i))) + str(i)))
